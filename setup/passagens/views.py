3576f2835be235e24575d4cc3b86f2b3b645ef5c
joseevilasio <jose_evilasio@outlook.com>
2020-08-17 21:40:16 -0300

    Curso alura, django forms

---
+++ setup/passagens/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from passagens.forms import PassagemForms
+
+def index(request):
+    form = PassagemForms()
+    contexto = {
+        'form' : form
+    }
+    return render(request, 'index.html', contexto)
+
+def revisao_consulta(request):
+    if request.method == 'POST':
+        form = PassagemForms(request.POST)
+        contexto = {
+        'form' : form
+        }
+        return render(request, 'minha_consulta.html', contexto)
+
